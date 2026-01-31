53c463fa36eed739af68c94f117d33cd963b1cfc
jobbo994 <jobbo994@gmail.com>
2026-01-31 23:37:50 +0100

    fix

--- Assets/Scripts/MotionCreator.cs
+++ Assets/Scripts/MotionCreator.cs
@@ -13,13 +13,13 @@ public class MotionCreator
     public static Motion get6A() {
         InputData[] inputs = { new InputData(NumpadDirection.Right, true) };
         InputData[] flippedInputs = { new InputData(NumpadDirection.Left, true) };
-        return new Motion(inputs, flippedInputs, 100, 50, 60, 0);
+        return new Motion("kick", inputs, flippedInputs, 100, 50, 60, 0);
     }
 
     public static Motion get2A()
     {
         InputData[] inputs = { new InputData(NumpadDirection.Down, true) };
         InputData[] flippedInputs = { new InputData(NumpadDirection.Down, true) };
-        return new Motion(inputs, flippedInputs, 60, 20, 40, 0, 5, 12);
+        return new Motion("lowHit",inputs, flippedInputs, 60, 20, 40, 0, 5, 12);
     }
 }
